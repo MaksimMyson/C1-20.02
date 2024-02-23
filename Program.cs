1df63fb8418e1adaafcb6b5a2f9de8ff8c43042d
MaksimMyson <maksimmyson33@gmail.com>
2024-02-23 21:19:37 +0200

    ex3

--- Program.cs
+++ Program.cs
@@ -1,10 +1,41 @@
-﻿namespace C1_20._02
+﻿using System;
+
+namespace C1_20._02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, C#");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Console.Write("Введіть шестизначне число:");
+            string input = Console.ReadLine();
+
+            if (input.Length != 6 || !IsNumeric(input))
+            {
+                Console.WriteLine("Ви ввели не шестизначне число.");
+                return;
+            }
+
+            string reversedNumber = ReverseNumber(input);
+            Console.WriteLine($"Число навпаки: {reversedNumber}");
+        }
+
+        private static string ReverseNumber(string input)
+        {
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        static bool IsNumeric(string str)
+        {
+            foreach (char c in str)
+            {
+                if (!char.IsDigit(c))
+                    return false;
+            }
+            return true;
         }
     }
 }
