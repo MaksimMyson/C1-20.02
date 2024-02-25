dcb6772eb087757632e1875b6d5179106f73009a
MaksimMyson <maksimmyson33@gmail.com>
2024-02-26 00:22:39 +0200

    exe6

--- Program.cs
+++ Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, C#");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine("Введіть температуру:");
+            double temp = Convert.ToDouble(Console.ReadLine());
+
+          Console.WriteLine("Введіть шкалу (C, F, K):");
+            string scale = Console.ReadLine();
+
+            switch (scale)
+            {
+                case "C":
+                    Console.WriteLine("Температура в Фаренгейтах: " + (temp * 9 / 5 + 32));
+                    Console.WriteLine("Температура в Кельвінах: " + (temp + 273.15));
+                    break;
+                case "F":
+                    Console.WriteLine("Температура в Цельсіях: " + ((temp - 32) * 5 / 9));
+                    Console.WriteLine("Температура в Кельвінах: " + ((temp - 32) * 5 / 9 + 273.15));
+                    break;
+                case "K":
+                    Console.WriteLine("Температура в Цельсіях: " + (temp - 273.15));
+                    Console.WriteLine("Температура в Фаренгейтах: " + ((temp - 273.15) * 9 / 5 + 32));
+                    break;
+                default:
+                    Console.WriteLine("Невірна шкала");
+                    break;
+            }
+            
         }
     }
 }
