3d82a8f4c172ad81dba1beba0d8116e244aa8506
MaksimMyson <maksimmyson33@gmail.com>
2024-02-25 21:30:25 +0200

    exe2

--- Program.cs
+++ Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, C#");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine("Введіть число:");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введіть відсоток:");
+            int percent = int.Parse(Console.ReadLine());
+
+           if (number > 0 && percent > 0)
+            {
+                Console.WriteLine($"Відсоток від числа {number} = {number * percent / 100}");
+            }
+            else
+            {
+                Console.WriteLine("Введіть додатні числа");
+            }
+            Console.WriteLine();
         }
+
+        
+
     }
 }
