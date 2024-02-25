104f64f75f587b50dbc2cf30bec1e400a5363fc6
MaksimMyson <maksimmyson33@gmail.com>
2024-02-26 00:43:49 +0200

    exe7

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
 ﻿namespace C1_20._02
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, C#");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Введіть перше число:");
+            int number1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введіть друге число:");
+            int number2 = int.Parse(Console.ReadLine());
+
+            // Нормалізація меж діапазону
+            int start = Math.Min(number1, number2);
+            int end = Math.Max(number1, number2);
+
+            Console.WriteLine($"Парні числа у діапазоні від {start} до {end}:");
+            for (int i = start; i <= end; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
