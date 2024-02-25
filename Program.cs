99207326e521a3ca23df7214a0457585958aef68
MaksimMyson <maksimmyson33@gmail.com>
2024-02-25 20:48:35 +0200

    exe1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,34 @@
-﻿namespace C1_20._02
+﻿using System;
+
+namespace C1_20._02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, C#");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine("Введіть число від 1 до 100:");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number < 1 || number > 100)
+            {
+                Console.WriteLine("Введене число не відповідає вимогам");
+                return;
+            }
+
+            if (number % 5 == 0 && number % 3 == 0) 
+            {
+                Console.WriteLine("Fizz\r\nBuzz");
+            }
+            else if (number % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
         }
     }
 }
