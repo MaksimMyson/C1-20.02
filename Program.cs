93551ca852ffa135afc4ea1a0f1eb12033e3dc08
MaksimMyson <maksimmyson33@gmail.com>
2024-02-25 23:10:18 +0200

    exe5

--- Program.cs
+++ Program.cs
@@ -1,10 +1,50 @@
-﻿namespace C1_20._02
+﻿using System.Globalization;
+
+namespace C1_20._02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, C#");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine("Введіть дату у форматі dd.MM.yyyy: ");
+            string date = Console.ReadLine();
+
+            if (DateTime.TryParseExact(date, "dd.MM.yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime parsedDate))
+            {
+                Console.WriteLine($"День тижня: {parsedDate.ToString("dddd", new CultureInfo("uk-UA"))}");
+                Console.WriteLine($"Пора року: {GetSeason(parsedDate.Month)}");
+            }
+            else
+            {
+                Console.WriteLine("Неправильний формат дати.");
+            }
+        }
+
+        static string GetSeason(int month)
+        {
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    return "Зима";
+                case 3:
+                case 4:
+                case 5:
+                    return "Весна";
+                case 6:
+                case 7:
+                case 8:
+                    return "Літо";
+                case 9:
+                case 10:
+                case 11:
+                    return "Осінь";
+                default:
+                    return "";
+            }
         }
     }
 }
